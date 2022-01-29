8f30adc220d741f5221d9cd72c8b996893bdfc77
Javoto <javi.lopez_@outlook.com>
2022-01-29 20:35:16 +0100

    revert: delete anination in HeroSection

--- src/components/organisms/HeroSection/components/Animation/index.tsx
+++ src/components/organisms/HeroSection/components/Animation/index.tsx
@@ -1,37 +1,7 @@
 import React from 'react';
 
-import styles from './styles.module.css';
-
 const Animation = () => {
-    return (
-        <div>
-            <div className={styles['objects']}>
-                <div className={styles['objects_computer']}></div>
-                <div className={styles['objects_table']}></div>
-                <div className={styles['objects_cup']}></div>
-                <div className={styles['smoke']}></div>
-                <div className={styles['objects_chair']}></div>
-            </div>
-            <div className={styles['box']}>
-                <div className={styles['box_1']}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className={styles['box_2']}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className={styles['box_3']}></div>
-            </div>
-        </div>
-    );
+    return <div></div>;
 };
 
 export default Animation;
